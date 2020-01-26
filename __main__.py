7e669b33db7912421248268a1d407bb22a3c2395
doglol99 <40714465+doglol99@users.noreply.github.com>
2020-01-26 12:29:05 +0000

    lawsuit setup is fully implemented

--- __main__.py
+++ __main__.py
@@ -7,14 +7,33 @@ lawsuitinprog=False
 Client = commands.Bot(commands.when_mentioned_or("!"),description="a bot for the lipidcraft server")
 
 
-@Client.command(description="sue someone")
+@Client.command(description="Sue someone")
 async def sue(ctx):
+    await ctx.message.delete(delay=0.5) # if no delay it doenst delete for some reason
     global lawsuitinprog
     if not lawsuitinprog:
         setupembed= await getsetupembed(ctx,"not set","not set")
         setupmsg = await ctx.send(embed=setupembed)
-        await ctx.send("please ping the person you would like to sue")
+        await ctx.send("please ping the person you would like to sue",delete_after=10)
         lawsuitinprog=True
+
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        msg = await Client.wait_for('message', check=check)
+        await msg.delete(delay=0.5)
+        setupembed = await getsetupembed(ctx, msg.content, "not set")
+        await setupmsg.edit(embed=setupembed)
+        await ctx.send("please state the reason you are suing this person", delete_after=10)
+
+        message = await Client.wait_for('message', check=check)
+        await message.delete(delay=0.5)
+        setupembed = await getsetupembed(ctx, msg.content, message.content)
+        await setupmsg.edit(embed=setupembed,delete_after=60)
+        plaintiff=ctx.author
+        defendant=msg.content
+        reason=message.content
+
     else:
         await ctx.send("lawsuit already in progress")
 
