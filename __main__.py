e4061c98814a21203a358ad71508e1b14b116e5b
doglol99 <40714465+doglol99@users.noreply.github.com>
2020-01-26 10:42:03 +0000

    renamed some fields,  checks if lawsuit in progress
    (forgot to commit earlier)

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import json
 import discord
 from discord.ext import commands
+lawsuitinprog=False
 
 
 Client = commands.Bot(commands.when_mentioned_or("!"),description="a bot for the lipidcraft server")
@@ -8,8 +9,14 @@ Client = commands.Bot(commands.when_mentioned_or("!"),description="a bot for the
 
 @Client.command(description="sue someone")
 async def sue(ctx):
-    setupembed= await getsetupembed(ctx)
-    game = await ctx.send(embed=setupembed)
+    global lawsuitinprog
+    if not lawsuitinprog:
+        setupembed= await getsetupembed(ctx,"not set","not set")
+        setupmsg = await ctx.send(embed=setupembed)
+        await ctx.send("please ping the person you would like to sue")
+        lawsuitinprog=True
+    else:
+        await ctx.send("lawsuit already in progress")
 
 
 @Client.event
@@ -20,16 +27,15 @@ async def on_ready():
 
 def main():
     with open('config.json') as f:
-
         config = json.load(f)
     Client.run(config["token"])
 
 
-async def getsetupembed(ctx):
-    creator = ctx.author.mention
-    defendantfield="defendant: "+"not set"
-    reasonfield="reason: "+"not set"
-    description=creator+"\n"+defendantfield+"\n"+reasonfield
+async def getsetupembed(ctx,defendant,reason):
+    plaintifffield="plaintiff:  "+ctx.author.mention
+    defendantfield="defendant: "+defendant
+    reasonfield="reason: "+reason
+    description=plaintifffield+"\n"+defendantfield+"\n"+reasonfield
     embed = discord.Embed(title="lawsuit setup", description=description, color=0x00ff00)
 
     return embed
